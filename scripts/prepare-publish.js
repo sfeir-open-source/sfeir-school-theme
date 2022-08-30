1cdd1129a8b382163147111d3f7f3d79907e0992
Anthony PENA <pena.a@sfeir.com>
2022-08-30 08:51:04 +0200

    feat: dist can be pack for publishing

---
+++ scripts/prepare-publish.js
@@ -0,0 +1,28 @@
+const shelljs = require("shelljs");
+const fs = require("node:fs");
+
+cleanupPackageJson();
+prepareFeather();
+
+function cleanupPackageJson() {
+  shelljs.rm("-f", "./dist/package.json");
+
+  const packageJson = require("../package.json");
+
+  // delete deps keys (because no deps after building it)
+  delete packageJson.dependencies;
+  delete packageJson.devDependencies;
+
+  // delete scripts (because useless for published package)
+  delete packageJson.scripts;
+
+  fs.writeFileSync(
+    "./dist/package.json",
+    JSON.stringify(packageJson, undefined, 2)
+  );
+}
+
+function prepareFeather() {
+  shelljs.cp("-rf", "./node_modules/feather-icons", "./dist/feather-icons");
+  shelljs.rm("-rf", "./dist/feather-icons/node_modules");
+}
