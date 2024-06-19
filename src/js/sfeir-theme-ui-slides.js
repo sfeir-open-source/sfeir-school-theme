cae7b18638455a73275ffa34bc958cb8e487bb64
jefbinomed <jean.francois.garreau@gmail.com>
2024-06-19 15:01:45 +0200

    core: add global ui to control theme and minor refactor

--- src/js/sfeir-theme-ui-slides.js
+++ src/js/sfeir-theme-ui-slides.js
@@ -1,4 +1,5 @@
 import { html, render } from 'lit-html';
+import { _handle_parameter } from './helper';
 
 /**
  * Basic css for the slide selector
@@ -7,7 +8,7 @@ const myCss = `
     .ui-slide-selector-container {
         position: absolute;
         width: 50%;
-        height: 60%;
+        height: 70%;
         top: 20%;
         left: 25%;
 
@@ -20,26 +21,45 @@ const myCss = `
         z-index: 100;
 
         display:grid;
-        grid-template-rows: 50px 1fr 100px;
+        grid-template-rows: 50px 1fr 50px 150px 100px 20px;
         gir-template-columns: 1fr;
+
+        #ui-slide-selector-close{
+            position: absolute;
+            top: 15px;
+            right: 15px;
+            cursor: pointer;
+            font-size: 1.5em;
+        }
 
         h1 {
             text-align: center;
             justify-self: center;
         }
 
+        .slide-selector {
+            overflow-y: auto;
+        }
         ul {
             list-style-type: none;
-            overflow-y: auto;
         }
         li {
             margin: 10px;
+        }
+
+        div.modes {
+            align-self: center;
+            text-align: center;
+            padding: 50px;
         }
 
         button{
             width: 150px;
             justify-self: center;
             margin: 20px;
+        }
+        span{
+            text-align: center;
         }
     }
 `;
@@ -48,6 +68,10 @@ const myCss = `
 export const SfeirThemeUiSelector = {
     state: {
         slides: [], // State of slides with element of type {prefix: string, path: string, index: number, check: boolean}
+        show: false, // Boolean to show the UI
+        theme: undefined, // Default theme applied
+        type: undefined, // Default type applied
+        language: undefined, // Default language applied
     },
     /**
      * Method to call to init the UI
@@ -66,6 +90,9 @@ export const SfeirThemeUiSelector = {
         document.body.addEventListener('keyup', (e) =>
             this.keyUpHandler(e, slides)
         );
+
+        // Init the modes (theme, lang, type)
+        this.initModes();
 
         // Define the slides to use (by default, whats come's in)
         let slidesToUse = slides;
@@ -96,8 +123,53 @@ export const SfeirThemeUiSelector = {
      * @param {*} e keyup event
      */
     keyUpHandler(e) {
-        if (e.key === 'c') {
+        if (e.key === 'c' && !this.state.show) {
+            this.state.show = true;
             this.resetOrCreateUI();
+        } else if ((e.key === 'Escape' || e.key === 'c') && this.state.show) {
+            this.closeUI();
+        }
+    },
+    /**
+     * Init the modes (Theme, Language, Type) according to parameters available in the session storage
+     */
+    initModes() {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const slidesElement = document.querySelector('.reveal .slides');
+
+        // We only get informations from url or inside html
+
+        this.state.theme = _handle_parameter(
+            urlParams,
+            'theme',
+            slidesElement,
+            'data-theme-slides',
+            undefined
+        );
+        this.state.type = _handle_parameter(
+            urlParams,
+            'type',
+            slidesElement,
+            'data-type-show',
+            undefined
+        );
+        this.state.language = _handle_parameter(
+            urlParams,
+            'data-lang',
+            slidesElement,
+            'data-lang',
+            undefined
+        );
+    },
+    /**
+     * Method called to close the UI
+     */
+    closeUI() {
+        this.state.show = false;
+        let elementSelection = document.querySelector('#ui-slide-selector');
+        if (elementSelection) {
+            elementSelection.style = 'display:none';
         }
     },
     /**
@@ -130,7 +202,7 @@ export const SfeirThemeUiSelector = {
      * Render the ui (creation of element if not exist, else reset the content of the element)
      */
     resetOrCreateUI() {
-        let elementSelection = document.querySelector('ui-slide-selector');
+        let elementSelection = document.querySelector('#ui-slide-selector');
         if (!elementSelection) {
             elementSelection = document.createElement('DIV');
             elementSelection.id = 'ui-slide-selector';
@@ -138,6 +210,7 @@ export const SfeirThemeUiSelector = {
                 'position:absolute; width:100%; height: 100%; top:0; left:0; z-index:100;';
             document.body.appendChild(elementSelection);
         }
+        elementSelection.style.display = this.state.show ? 'grid' : 'none';
         // We finally init the UI
         this.initUI(elementSelection);
     },
@@ -155,15 +228,53 @@ export const SfeirThemeUiSelector = {
                     ${myCss}
                 </style>
                 <div class="ui-slide-selector-container">
+                    <div
+                        id="ui-slide-selector-close"
+                        @click="${() => this.closeUI()}">
+                        ✖️
+                    </div>
                     <h1>Slide selector</h1>
-                    <ul>
-                        ${treeArray.map(([key, value]) =>
-                            this.renderTreeElement(key, value)
-                        )}
-                    </ul>
-                    <button @click="${() => this.applySlides()}">
+                    <div class="slide-selector">
+                        <ul>
+                            ${treeArray.map(([key, value]) =>
+                                this.renderTreeElement(key, value)
+                            )}
+                        </ul>
+                    </div>
+                    <h1>Modes (type / theme / language)</h1>
+                    <div class="modes">
+                        <label for="mode-theme">Theme</label>
+                        <input
+                            type="text"
+                            id="mode-theme"
+                            placeholder="(empty default 'school')"
+                            value="${this.state.theme
+                                ? this.state.theme
+                                : undefined}" />
+                        <label for="mode-language">Language</label>
+                        <input
+                            type="text"
+                            id="mode-language"
+                            placeholder="(empty default 'FR')"
+                            value="${this.state.language
+                                ? this.state.language
+                                : undefined}" />
+                        <label for="mode-slides">Type</label>
+                        <input
+                            type="text"
+                            id="mode-type"
+                            placeholder="(empty default 'prez')"
+                            value="${this.state.type
+                                ? this.state.type
+                                : undefined}" />
+                    </div>
+                    <button @click="${() => this.applyConfiguration()}">
                         Validate selection
                     </button>
+                    <span
+                        >Close this window by pressing again 'c', clicking on
+                        cross or validating the selection</span
+                    >
                 </div>
             `,
             element
@@ -240,11 +351,36 @@ export const SfeirThemeUiSelector = {
         );
         this.resetOrCreateUI();
     },
+
+    extractModes() {
+        const theme = document.querySelector('#mode-theme').value || undefined;
+        const language =
+            document.querySelector('#mode-language').value || undefined;
+        const type = document.querySelector('#mode-type').value || undefined;
+        return { theme, language, type };
+    },
+
     /**
      * Handler method that deals with the apply button. it will refresh the page
      */
-    applySlides() {
-        // Refresh the page
-        location.reload();
+    applyConfiguration() {
+        const { theme, language, type } = this.extractModes();
+
+        // Construct the url
+        let newUrl = '';
+        const addUrl = (url, key, value) =>
+            value && url
+                ? `${url}&${key}=${value}`
+                : value
+                ? `${url}?${key}=${value}`
+                : url;
+
+        newUrl = addUrl(newUrl, 'theme', theme);
+        newUrl = addUrl(newUrl, 'data-lang', language);
+        newUrl = addUrl(newUrl, 'type', type);
+
+        // Apply the new url
+        history.pushState({ theme, language, type }, '', newUrl);
+        history.go(0);
     },
 };
